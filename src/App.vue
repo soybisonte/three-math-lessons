0d61e61b23112ffcacbd80563a4fc24b8dd4b209
Rodriguez Martinez Hector Heriberto <mb82922@MB82922MAC.local>
2020-02-11 16:55:19 -0600

    inital setup

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,4 @@
   </div>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+<style lang="stylus" src="@/styles/app.styl"></style>
